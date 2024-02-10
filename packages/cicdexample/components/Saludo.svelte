b64a2421e11be491b42adf9c0a2a5df23e8d83e8
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-09 21:38:34 -0600

    se elimina

--- packages/cicdexample/components/Saludo.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  const date = new Date();
-  export let nombre = 'Mundo';
-</script>
-
-<h1>Hola {nombre}, la hora exacta es {date.toLocaleTimeString()}!</h1>
-
-<style lang="postcss">
-</style>
--- packages/cicdexample/components/tipos.ts
+++
@@ -1,4 +0,0 @@
-export type Persona = {
-  nombre: string;
-  apellido: string;
-};
